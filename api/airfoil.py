77eb272bd0a85936f768810e288929dadd8a2f97
Filip Hedman <hedman.filip@gmail.com>
2016-10-06 11:39:19 +0200

    Run command in shell from code

--- api/airfoil.py
+++ api/airfoil.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import subprocess as sp
 
 app = Flask(__name__)
 
@@ -15,13 +16,20 @@ def get_flow():
     time = 1
     mesh = "./test_mesh.xml"
 
-    # TODO Run the calculation script here for real result data
-    # TODO Spawn docker instance via docker-py
+    # TODO Run the calculation script here for real result data, i.e.
+    # cmd = './airfoil'
+    # params = [cmd, num_samples, visc, speed, time, mesh]
+    cmd = 'echo'
+    params = [cmd, 'Hello Airfoil!']
+    # Runs a specified program on command line
+    output = sp.check_output(params)
+    
+    # TODO Spawn docker instance via docker-py?
 
     lift_force = 3.0
     drag_force = 2.12
     
-    return jsonify({'result': {'lift_force': lift_force, 'drag_force': drag_force}})
+    return jsonify({'result': {'lift_force': lift_force, 'drag_force': drag_force, 'output': output}})
 
 if __name__ == '__main__':
     app.run(debug=True)
