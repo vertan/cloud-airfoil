e73a2be99e42f9abac9c08d8d2a952f671146e9b
shoulderface <shoulder_92@hotmail.com>
2016-10-18 05:58:12 -0700

    fix airfoil_helper

--- airfoil_helper.py
+++ airfoil_helper.py
@@ -3,10 +3,13 @@ from tasks import get_flow
 def get_flow_result(angle_start, angle_stop, n_angles, n_nodes,
                     n_levels, num_samples, visc, speed, time, run_id):
     step = (angle_stop-angle_start)/n_angles
+
+    results = []
     
-    for i,angle in enumerate(range(angle_start, angle_stop+1, step)):
-        results[i] = get_flow.delay(str(angle), str(n_nodes), str(n_levels),
-                              str(num_samples), str(visc), str(speed), str(time), str(run_id))
+    for angle in range(angle_start, angle_stop+1, step):
+        results.append(get_flow.delay(str(angle), str(n_nodes), str(n_levels),
+                              str(num_samples), str(visc), str(speed), str(time), str(run_id)))
+        
     for i,angle in enumerate(range(angle_start, angle_stop+1, step)):
         results[i] = results[i].get()
     
