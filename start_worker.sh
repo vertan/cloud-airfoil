6378f9ec9978f82f295cb80c294bdf7190bd45d4
shoulderface <shoulder_92@hotmail.com>
2016-10-18 05:49:54 -0700

    updated broker cfg and start_worker

--- start_worker.sh
+++ start_worker.sh
@@ -5,6 +5,7 @@ sleep 20
 scp -o "StrictHostKeyChecking no" -i $HOME/cloud-airfoil/proj.key $HOME/config.py ubuntu@$FLOATING_IP:~
 ssh -t -i $HOME/cloud-airfoil/proj.key ubuntu@$FLOATING_IP <<-"ENDSSH"
 mv proj.key .ssh/id_rsa
+sudo chmod 600 .ssh/id_rsa
 git clone git@github.com:vertan/cloud-airfoil.git
 screen -d -m celery -A tasks worker --loglevel=info
 screen -d -m celery -A tasks worker --loglevel=info
